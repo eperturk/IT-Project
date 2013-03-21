9b4d03551f3bc8d64b8cb04945320de192eb80e0
Erol Erturk <erol@ubicare.com>
2013-03-21 16:43:55 -0400

    App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+It::Application.config.secret_token = '5f48ac2358435ea68fb74577d5ef01166e083b107e909c20c4ef216496cb693e756d201afac6ccbbd25f32032da8ea5aa85e574837f7d8c2a0653599f261cb16'
